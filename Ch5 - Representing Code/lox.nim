67eb4caf67eb733074e230f1e50c33a052c4a742
Edward Kish <erk52@cornell.edu>
2023-10-24 00:02:53 -0400

    Add files via upload
    This is where I gave up last time I tried this in Java because I hate Java's object oriented nonsense. A syntax tree makes perfect sense. This visitor / accept pattern makes everything way more confusing. Nim lets me have just one nice print function, not an entire class with new methods for each possible type.

---
+++ Ch5 - Representing Code/lox.nim
@@ -0,0 +1,51 @@
+import os
+import std/strformat
+import token
+import scanner
+import expressions
+import ereport
+
+var script: string
+var hadError: bool = false
+  
+#[
+proc scanTokens(s: string): seq[Token] = 
+  var result: seq[Token] = @[]
+  result.add(Token(literal: s))
+  return result
+]#
+proc run(s: string) = 
+  var scan = Scanner(source: s)
+  var tokens = scan.scanTokens()
+  for t in tokens:
+    echo t.toString()
+
+proc runFile(f: string) = 
+  echo fmt"Loading script {f}"
+  script = readFile(f)
+  run(script)
+  return
+  
+proc runPrompt() = 
+  echo "Welcome to Lox REPL"
+  while true:
+    write(stdout, "-> ")
+    let inp = readLine(stdin)
+    if inp == "":
+      break
+    run(inp)
+    hadError = false
+  return
+
+proc main() = 
+  let args = commandLineParams()
+  if len(args) > 1:
+    echo "Too many arguments."
+    return
+  elif len(args) == 1:
+    runFile(args[0])
+  else:
+    runPrompt()
+    
+  
+main()
